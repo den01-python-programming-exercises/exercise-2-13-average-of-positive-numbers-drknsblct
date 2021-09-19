fa3128ed55dee096b1857d93a5d2ac77e5e94e0b
drknsblct <57183092+drknsblct@users.noreply.github.com>
2021-09-19 04:22:29 +0300

    Update exercise.py

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,27 @@
 def main():
     #write your code below this line
+    sum = 0
+    positive = 0
+    
+    while True:
+        num = int(input('Give a number:'))
+        
+        if num == 0:
+            break
+        if num > 0:
+            positive += 1
+            
+        
+        sum += num
+        
+    if sum == 0:
+        print('Cannot calculate the average')
+    else:
+        print(positive/sum)
+        
+    print('Number of numbers: ' + str(count))
+    print('Sum of numbers: ' + str(sum))
+    
 
 if __name__ == '__main__':
     main()
